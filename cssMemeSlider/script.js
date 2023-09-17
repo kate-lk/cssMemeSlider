fe20c30db826c1d720b21eefdb31bd992f8db3a9
Kate Lupashtsian <kate.lupashtsian@gmail.com>
2023-09-18 01:03:06 +0300

    feat: add adaptive

--- cssMemeSlider/script.js
+++ cssMemeSlider/script.js
@@ -1,43 +1,41 @@
-const sliderLine = document.querySelector('.image-line');
-const sliderImg = document.querySelectorAll('.image-slid');
-const sliderControl = document.querySelectorAll('.control');
-const sliderText = document.querySelectorAll('.text');
+const sliderLine = document.querySelector(".image-line");
+const sliderImg = document.querySelectorAll(".image-slid");
+const sliderControl = document.querySelectorAll(".control");
+const sliderControls = document.querySelectorAll(".controls");
+const sliderText = document.querySelectorAll(".text");
 
 let sliderCount = 0;
 let sliderWidth;
 
-window.addEventListener('resize', showSlide);
+window.addEventListener("resize", showSlide);
 
 function showSlide() {
-    sliderWidth = document.querySelector('.image-wrapper').offsetWidth;
-    sliderLine.computedStyleMap.width = sliderWidth * sliderImg.length + 'px';
-    sliderImg.forEach(item => item.style.width = sliderWidth + 'px');
-
-
+  sliderWidth = document.querySelector(".image-wrapper").offsetWidth;
+  sliderLine.computedStyleMap.width = sliderWidth * sliderImg.length + "px";
+  sliderImg.forEach((item) => (item.style.width = sliderWidth + "px"));
 }
 
 showSlide();
 
-function rollSlider () {
-    sliderLine.style.transform = `translateX(${-sliderCount * sliderWidth}px)`;
+function rollSlider() {
+  sliderLine.style.transform = `translateX(${-sliderCount * sliderWidth}px)`;
 }
 
 function thisText(index) {
-    sliderText.forEach(item => item.classList.remove('active-text'));
-    sliderText[index].classList.add('active-text');
-
+  sliderText.forEach((item) => item.classList.remove("active-text"));
+  sliderText[index].classList.add("active-text");
 }
 
 function thisSlide(index) {
-    sliderControl.forEach(item => item.classList.remove('active-control'));
-    sliderControl[index].classList.add('active-control');
+  sliderControl.forEach((item) => item.classList.remove("active-control"));
+  sliderControl[index].classList.add("active-control");
 }
 
-sliderControl.forEach((control, index) => {
-    control.addEventListener('click', () => {
-        sliderCount = index;
-        rollSlider();
-        thisSlide(sliderCount);
-        thisText(sliderCount);
-    })
-})
+sliderControls.forEach((control, index) => {
+  control.addEventListener("click", () => {
+    sliderCount = index;
+    rollSlider();
+    thisSlide(sliderCount);
+    thisText(sliderCount);
+  });
+});
